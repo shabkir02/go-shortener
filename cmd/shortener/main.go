44c3ec5a30844c289ce85089acd411c8ba912b87
MacBook (Kirill) <macbook@MacBook-Pro-2.local>
2023-05-02 22:36:23 +0600

    feat: server struct

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,55 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Middleware func(http.Handler) http.Handler
+
+func Conveyor(h http.Handler, middlewares ...Middleware) http.Handler {
+	for _, middleware := range middlewares {
+		h = middleware(h)
+	}
+	return h
+}
+
+func middleware(next http.Handler) http.Handler {
+	// собираем Handler приведением типа
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header.Get("Content-Type"))
+		// разрешаем запросы cross-domain
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		fmt.Println(string(b))
+
+		w.Write([]byte(b))
+		w.WriteHeader(http.StatusCreated)
+	case "GET":
+		w.Header().Set("Location", "http://localhost:8080")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	handler1 := http.HandlerFunc(GetHandler)
+	mux.Handle("/", Conveyor(handler1, middleware))
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
